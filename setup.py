90a02569cac531db579f3f9944048e28c8e4c5b8
Richard Harding <rharding@mitechie.com>
2012-05-11 21:18:47 -0400

    Prep for 0.1.1 release

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = open(os.path.join(here, 'README.rst')).read()
 NEWS = open(os.path.join(here, 'NEWS.txt')).read()
 
 
-version = '0.1.0'
+version = '0.1.1'
 install_requires = [
     # List your project dependencies here.
     # For more details, see:
