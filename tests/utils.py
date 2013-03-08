e3b6ee2fd6993d10f9ddbe6477a1ff17543e4f26
Mišo Belica <miso.belica@gmail.com>
2013-03-08 17:46:18 +0100

    Suppress warning "ResourceWarning: unclosed file"

--- tests/utils.py
+++ tests/utils.py
@@ -6,9 +6,13 @@ TEST_DIR = path.dirname(__file__)
 
 def load_snippet(filename):
     """Helper to fetch in the content of a test snippet"""
-    return open(path.join(TEST_DIR, 'test_snippets', filename)).read()
+    file_path = path.join(TEST_DIR, 'test_snippets', filename)
+    with open(file_path) as file:
+        return file.read()
 
 
 def load_article(filename):
     """Helper to fetch in the content of a test article"""
-    return open(path.join(TEST_DIR, 'test_articles', filename)).read()
+    file_path = path.join(TEST_DIR, 'test_articles', filename)
+    with open(file_path) as file:
+        return file.read()
