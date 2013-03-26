05d2230015e085cba408474539f997f7fecd2f91
Mišo Belica <miso.belica@gmail.com>
2013-03-26 19:55:50 +0100

    Load articles/snippets as binary strings

--- tests/utils.py
+++ tests/utils.py
@@ -12,12 +12,12 @@ TEST_DIR = abspath(dirname(__file__))
 def load_snippet(file_name):
     """Helper to fetch in the content of a test snippet."""
     file_path = join(TEST_DIR, "data/snippets", file_name)
-    with open(file_path) as file:
+    with open(file_path, "rb") as file:
         return file.read()
 
 
 def load_article(file_name):
     """Helper to fetch in the content of a test article."""
     file_path = join(TEST_DIR, "data/articles", file_name)
-    with open(file_path) as file:
+    with open(file_path, "rb") as file:
         return file.read()
